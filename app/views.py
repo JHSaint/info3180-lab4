6e8522afd9c331390b262ea18e5e558c350c5a13
JHSaint <jordan.hewitta7@gmail.com>
2020-02-29 15:09:05 -0500

    Part 1 Complete

--- app/views.py
+++ app/views.py
@@ -8,8 +8,7 @@ import os
 from app import app
 from flask import render_template, request, redirect, url_for, flash, session, abort
 from werkzeug.utils import secure_filename
-
-
+from .forms import UploadForm
 ###
 # Routing for your application.
 ###
@@ -33,11 +32,20 @@ def upload():
 
     # Instantiate your form class
 
+    form = UploadForm()
     # Validate file upload on submit
+    if request.method == "GET":
+        return render_template('upload.html', form = form)
+
     if request.method == 'POST':
         # Get file data and save to your uploads folder
-
-        flash('File Saved', 'success')
+        if form.validate_on_submit() == True:
+            f = form.file_upload.data
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(
+                app.config['UPLOAD_FOLDER'], filename
+            ))
+            flash('File Saved', 'success')
         return redirect(url_for('home'))
 
     return render_template('upload.html')
